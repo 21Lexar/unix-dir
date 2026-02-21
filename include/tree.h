e831ab4f114ece037d982f216f80ca97b5b26964
21Lexar <the-charon@hotmail.com>
2026-02-21 15:08:58 +0500

    Init Tree Header

---
+++ include/tree.h
@@ -0,0 +1,15 @@
+#ifndef TREE_H
+#define TREE_H
+
+#include "struct.h"
+
+
+int insert_file(directory *parent_directory, file *file_to_insert){
+        if(file_to_insert == NULL){
+                return -1;
+        }
+        
+        
+}
+
+#endif
